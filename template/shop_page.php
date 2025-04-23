0fd920475b543bada35fef0433780f580af21921
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-23 21:57:02 +0600

    xyf

--- template/shop_page.php
+++ template/shop_page.php
@@ -5,33 +5,7 @@
 
 get_header(); ?>
 
-<section id="product_area">
-    <div class="container">
-      <div class="row">
-        <?php 
-        query_posts('post_type=custom_product&post_status=publish&posts_per_page=0&order=ASC&paged='. get_query_var('post')); 
 
-        if(have_posts()) :
-          while(have_posts()) : the_post(); 
-        ?>
-        <div class="col-md-4">
-          <div class="child_service">
-          <h2><?php the_title(); ?></h2>
-          <?php echo the_post_thumbnail('custom_product') ?>
-          <?php the_excerpt(  ); ?>
-            <div class="price"><?php echo get_post_meta(get_the_ID(), 'product_price', true); ?></div>
-            <div class="button_area">
-              <a href="<?php echo get_post_meta(get_the_ID(), 'product_link', true); ?>" class="btn btn-primary">Buy Now</a>
-          </div>
-        </div>
-
-        <?php 
-          endwhile;
-          endif;
-        ?>
-      </div>
-    </div>
-  </section>
 
 
 <?php get_footer(); ?>
